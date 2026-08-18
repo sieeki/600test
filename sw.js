639860bc47a3eff85fa2e2c097caea443fd4776a
SIEEKI 3 <97383131+sieeki@users.noreply.github.com>
2026-08-18 12:42:43 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-// sw.js
 self.addEventListener('push', function(event) {
     const data = event.data ? event.data.json() : {};
-    const title = data.title || 'Квартира';
+    const title = data.title || 'air chat';
     const options = {
         body: data.body || 'Новое уведомление!',
-        icon: 'https://via.placeholder.com/192'
+        icon: 'https://via.placeholder.com/192',
+        badge: 'https://via.placeholder.com/192'
     };
 
     event.waitUntil(
